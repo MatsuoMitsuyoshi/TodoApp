878a755174b413d46cece079b86039ec1545eb78
mitsuyoshi <m123matsuo@icloud.com>
2019-03-25 20:09:01 +0900

    Create Add Todo and Fiter Todo

--- App.js
+++ App.js
@@ -1,12 +1,144 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Platform,
+  ScrollView,
+  FlatList,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  AsyncStorage,
+  // 1: TouchableOpacityを追加
+  TouchableOpacity,
+} from 'react-native';
+
+const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
+const TODO = "@todoapp.todo"
+
+// 2: TODOアイテムの Functional Component
+const TodoItem = (props) => {
+  let textStyle = styles.todoItem
+  if (props.done === true) {
+    textStyle = styles.todoItemDone
+  }
+  return (
+    <TouchableOpacity onPress={props.onTapTodoItem}>
+      <Text style={textStyle}>{props.title}</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      todo: [],
+      currentIndex: 0,
+      inputText: "",
+      filterText: "",
+    }
+  }
+
+  componentDidMount() {
+    this.loadTodo()
+  }
+
+  loadTodo = async () => {
+    try {
+      const todoString = await AsyncStorage.getItem(TODO)
+      if (todoString) {
+        const todo = JSON.parse(todoString)
+        const currentIndex = todo.length
+        this.setState({todo: todo, currentIndex: currentIndex})
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  saveTodo = async (todo) => {
+    try {
+      const todoString = JSON.stringify(todo)
+      await AsyncStorage.setItem(TODO, todoString)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  onAddItem = () => {
+    const title = this.state.inputText
+    if (title == "") {
+      return
+    }
+    const index = this.state.currentIndex + 1
+    const newTodo = {index: index, title: title, done: false}
+    const todo = [...this.state.todo, newTodo]
+    this.setState({
+      todo: todo,
+      currentIndex: index,
+      inputText: ""
+    })
+    this.saveTodo(todo)
+  }
+
+  // 3: TODOリストをタップした時の処理
+  onTapTodoItem = (todoItem) => {
+    const todo = this.state.todo
+    const index = todo.indexOf(todoItem)
+    todoItem.done = !todoItem.done
+    todo[index] = todoItem
+    this.setState({todo: todo})
+    this.saveTodo(todo)
+  }
+
   render() {
+    const filterText = this.state.filterText
+    let todo = this.state.todo
+    if (filterText !== "") {
+      todo = todo.filter(t => t.title.includes(filterText))
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <View style={styles.filter}>
+          <TextInput
+            onChangeText={(text) => this.setState({filterText: text})}
+            value={this.state.filterText}
+            style={styles.inputText}
+            placeholder="Type filter text"
+          />
+        </View>
+        <ScrollView style={styles.todolist}>
+          { /* 4: FlatListの修正 */ }
+          <FlatList data={todo}
+            extraData={this.state}
+            renderItem={({item}) =>
+              <TodoItem
+                title={item.title}
+                done={item.done}
+                onTapTodoItem={() => this.onTapTodoItem(item)}
+                />
+            }
+            keyExtractor={(item, index) => "todo_" + item.index}
+          />
+        </ScrollView>
+        <View style={styles.input}>
+          <TextInput
+            onChangeText={(text) => this.setState({inputText: text})}
+            value={this.state.inputText}
+            style={styles.inputText}
+          />
+          <Button
+            onPress={this.onAddItem}
+            title="Add"
+            color="#841584"
+            style={styles.inputButton}
+          />
+        </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -15,7 +147,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: STATUSBAR_HEIGHT,
+  },
+  filter: {
+    height: 30,
+  },
+  todolist: {
+    flex: 1
+  },
+  input: {
+    height: 30,
+    flexDirection: 'row',
+  },
+  inputText: {
+    flex: 1,
+  },
+  inputButton: {
+    width: 100,
+  },
+  // 5: TODO表示用のスタイル
+  todoItem: {
+    fontSize: 20,
+    backgroundColor: "white",
+  },
+  todoItemDone: {
+    fontSize: 20,
+    backgroundColor: "red",
   },
 });
